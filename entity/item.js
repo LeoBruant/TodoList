52576177a5539e38472b58ce386ec281cea3fa15
xaless <hsuwang75@gmail.com>
2023-04-26 15:04:14 +0200

    fix items and add test password

---
+++ entity/item.js
@@ -0,0 +1,19 @@
+class Item {
+    constructor(name,content,date){
+        this.name = name
+        this.content = content
+        this.date = date
+    }
+        
+    isValid(todoLists){
+        if (
+            (this.content.length <= 1000) ||
+            (
+              todoLists.length &&
+              todoLists[0].length &&
+              Date.parse(Date.now()) < Date.parse(todoLists[0][todoLists[0].length - 1].date) + 30 * 60 * 1000
+            )
+          ) return true
+        return false
+    }
+}
