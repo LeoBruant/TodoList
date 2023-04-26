1cfc4b08c1fe641caa017ddbf7d385af942c624a
xaless <hsuwang75@gmail.com>
2023-04-26 14:24:24 +0200

    class test + test email

---
+++ entity/items.js
@@ -0,0 +1,11 @@
+class Items {
+    constructor(name,content,dcreation){
+        this.name = name
+        this.content = content
+        this.dcreation = dcreation
+    }
+
+    isValid(){
+        
+    }
+}
