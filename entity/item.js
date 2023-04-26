b404014ba4b62715a0ce99e47cc773cf91374411
xaless <hsuwang75@gmail.com>
2023-04-26 18:57:09 +0200

    fix class item

--- entity/item.js
+++ entity/item.js
@@ -6,14 +6,13 @@ class Item {
     }
         
     isValid(todoLists){
-        if (
+        return (
             (this.content.length <= 1000) ||
             (
               todoLists.length &&
               todoLists[0].length &&
               Date.parse(Date.now()) < Date.parse(todoLists[0][todoLists[0].length - 1].date) + 30 * 60 * 1000
             )
-          ) return true
-        return false
+          )
     }
 }
